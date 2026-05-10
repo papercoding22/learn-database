6b7171f0c9e7c8e804cef275e35f031fd547fbee
papercoding22 <john.ly997@gmail.com>
2026-05-10 13:08:42 +0700

    Add SQL exercises for QuanLyGiaoVu assignment, covering questions 6 to 10

---
+++ homework/Week7/bai03.sql
@@ -0,0 +1,18 @@
+-- Sinh viên hoàn thành Phần III bài tập QuanLyGiaoVu từ câu 6 đến câu 10.
+
+-- 6. Tìm tên những môn học mà giáo viên có tên “Tran Tam Thanh” dạy trong học kỳ 1 năm 2006.
+
+
+
+-- 7. Tìm những môn học (mã môn học, tên môn học) mà giáo viên chủ nhiệm lớp “K11” dạy trong học
+-- kỳ 1 năm 2006.
+
+
+-- 8. Tìm họ tên lớp trưởng của các lớp mà giáo viên có tên “Nguyen To Lan” dạy môn “Co So Du Lieu”.
+
+
+-- 9. In ra danh sách những môn học (mã môn học, tên môn học) phải học liền trước môn “Co So Du Lieu”.
+
+
+-- 10. Môn “Cau Truc Roi Rac” là môn bắt buộc phải học liền trước những môn học (mã môn học, tên
+-- môn học) nào.
